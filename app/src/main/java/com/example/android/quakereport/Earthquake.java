a64bb6bba7100d017aad913d6c279107ed63c671
haozheng <hza89@sfu.ca>
2018-01-27 21:09:20 -0800

    update 1

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,41 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by zhenghao on 2018-01-27.
+ */
+
+public class Earthquake {
+    private double mMagnitude;
+
+    private String mLocation;
+
+    private long mTimeInMilliseconds;
+
+    private String mUrl;
+
+    public double getMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public Earthquake(double magnitude, String location, long
+                      timeInMilliseconds, String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+
+}
